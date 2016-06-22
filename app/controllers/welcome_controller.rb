62415f360af3e2abd8dfe66edf169304fca702de
Wen-Tien Chang <ihower@gmail.com>
2016-06-22 11:37:14 +0800

    Add json demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,9 @@ class WelcomeController < ApplicationController
         render :text => "<h3>AWESOME!!! #{Time.now}</h3>"
       }
       format.js  # something.js.erb
+      format.json {
+        render :json => { :foobar => Time.now }
+      }
     end
 
   end
