38c15651850d45faa647350869e633ebec3ada8f
Wen-Tien Chang <ihower@gmail.com>
2016-07-19 21:12:21 +0800

    Integrate current_user

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -8,6 +8,8 @@ class MessagesChannel < ApplicationCable::Channel
 
     @message = Message.new( :content => data["content"] )
 
+    @message.user = current_user
+
     if @message.save
       html = ApplicationController.renderer.render( :partial => "messages/message", :locals => { :message => @message } )
       ActionCable.server.broadcast "public_channel", { :html => html }
